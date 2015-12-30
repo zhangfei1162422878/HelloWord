4d978ffb09cf75958858039d52685fac13740395
qianfeng <qianfeng@Cater.local>
2015-12-30 16:08:54 +0800

    1517提交代码到Developer分支

---
+++ AVPlayerDemo/Myclass.h
@@ -0,0 +1,13 @@
+//
+//  Myclass.h
+//  AVPlayerDemo
+//
+//  Created by qianfeng on 15/12/30.
+//  Copyright © 2015年 张鹏飞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Myclass : NSObject
+
+@end
---
+++ AVPlayerDemo/Myclass.m
@@ -0,0 +1,13 @@
+//
+//  Myclass.m
+//  AVPlayerDemo
+//
+//  Created by qianfeng on 15/12/30.
+//  Copyright © 2015年 张鹏飞. All rights reserved.
+//
+
+#import "Myclass.h"
+
+@implementation Myclass
+
+@end
